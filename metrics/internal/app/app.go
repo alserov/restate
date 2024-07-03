e3d7813392965eb187373a8a1ec7d196c2c8f62a
Alex Rokachev <alserokachev@gmail.com>
2024-07-03 21:05:56 +0300

    registered metrics, updated workers

--- metrics/internal/app/app.go
+++ metrics/internal/app/app.go
@@ -7,6 +7,9 @@ import (
 	"github.com/alserov/restate/metrics/internal/log"
 	"github.com/alserov/restate/metrics/internal/workers"
 	_ "github.com/joho/godotenv/autoload"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
 	"os/signal"
 	"syscall"
 )
@@ -17,15 +20,30 @@ const (
 
 func MustStart(cfg *config.Config) {
 	lg := log.NewLogger(cfg.Env, log.KindZap)
+	reg := prometheus.NewRegistry()
+
+	// workers init
+	var collectors []prometheus.Collector
+
+	systemWorker := workers.NewWorker(
+		workers.System,
+		async.NewConsumer(async.Kafka, cfg.Broker.Addr, cfg.Broker.Topics.Metrics),
+		&collectors,
+	)
+
+	reg.MustRegister(collectors...)
+
+	// endpoint for prometheus
+	m := http.NewServeMux()
+	m.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{}))
 
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	ctx = log.WithLogger(ctx, lg)
+
 	run(func() {
-		go workers.NewWorker(
-			workers.System,
-			async.NewConsumer(async.Kafka, cfg.Broker.Addr, cfg.Broker.Topics.Metrics),
-		).Run(log.WithLogger(ctx, lg), systemWorkers)
+		systemWorker.Run(ctx, systemWorkers)
 
 		lg.Info("server is running", nil)
 	})
--- metrics/internal/workers/worker.go
+++ metrics/internal/workers/worker.go
@@ -3,18 +3,20 @@ package workers
 import (
 	"context"
 	"github.com/alserov/restate/metrics/internal/async"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 type Worker interface {
+	Metrics() []prometheus.Collector
 	Run(ctx context.Context, workersAmount int)
 }
 
-func NewWorker(t WorkerType, consumer async.Consumer) Worker {
+func NewWorker(t WorkerType, consumer async.Consumer, colls *[]prometheus.Collector) Worker {
 	switch t {
 	case Business:
 		return nil
 	case System:
-		return NewSystemWorker(consumer)
+		return NewSystemWorker(consumer, colls)
 	default:
 		panic("invalid worker type")
 	}
